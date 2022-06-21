9077ce5e3fd67940c70952f25ba2d2eaad3067bf
Rui Guo <guojiarui@gmail.com>
2022-06-21 04:09:52 -0700

    render latitude longitude list fail

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       city: '',
+      lat:'',
+      lon:'',
       cityList: [],
       cityData: {},
       error: false,
@@ -43,19 +45,35 @@ class App extends React.Component {
     // make my request to my API
     let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
     let cityInfo = await axios.get(url);
-    console.log(cityInfo.data[0]);
+    console.log(cityInfo.data);
   }
 
   render() {
+  
+    // let cityLat = this.state.lat.map((city, idx) => {
+    //   return <li key={idx}>{city.lat}</li>;
+    // })
+    // let cityLon = this.state.lon.map((city, idx) => {
+    //   return <li key={idx}>{city.lon}</li>;
+    // })
     return (
       <>
         <h1>Data from an API</h1>
         <form onSubmit={this.handleCitySubmit}>
-          <label>Pick a City:
+          <label>Input a City:
             <input type="text" onInput={this.handleCityInput} />
           </label>
           <button type="submit">Explore!</button>
         </form>
+        {this.state.error
+          // render the error message:
+          ? <p>{this.state.errorMessage}</p>
+          // render the star wars list:
+          : <ul>
+            {/* {cityLat}, {cityLon} */}
+            {/* {this.cityInfo.data} */}
+          </ul>
+        }
       </>
     );
   }
