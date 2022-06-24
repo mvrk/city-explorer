635d7b2aa4a4a8130c6c5a69e767d73079d631f8
Rui Guo <guojiarui@gmail.com>
2022-06-23 19:22:26 -0700

    lab8 working code.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // import Card from 'react-bootstrap/Card';
 // import Image from 'react-bootstrap/Image';
 import Weather from './Weather.js';
+import Movies from './Movies.js';
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -11,10 +12,12 @@ class App extends React.Component {
       city: '',
       cityData: {},
       cityMap: '',
+      cityName: '',
       datetime: '',
       description: '',
       weatherData: [],
       error: false,
+      moviesData: [],
       errorMessage: '',
       displayCity: false
 
@@ -33,23 +36,26 @@ class App extends React.Component {
     try {
       let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
       let cityData = await axios.get(url);
-
+      console.log(cityData.data[0]);
       this.setState({
         displayCity: true,
         cityData: cityData.data[0],
+        // cityName: cityData.data[0].display_name,
         lat: cityData.data[0].lat,
         lon: cityData.data[0].lon,
 
         cityMap: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityData.data[0].lat},${cityData.data[0].lon}&zoom=12`
       });
-      
+
       this.handleGetWeather(cityData.data[0].lat, cityData.data[0].lon);
+      this.handleGetMovie(this.state.city);
     }
 
     catch (error) {
       this.setState({
         error: true,
         errorMessage: `An Error Occurred: ${error.response.status}`
+        // errorMessage: `An Error Occurred: ${error.response}`
       });
     }
   };
@@ -72,6 +78,27 @@ class App extends React.Component {
       });
     }
   }
+
+
+  handleGetMovie = async () => {
+    let url = `${process.env.REACT_APP_SERVER}/movies?searchQuery=${this.state.city}`;
+
+    try {
+      let movieResponse = await axios.get(url);
+      console.log(movieResponse.data);
+      this.setState({
+        moviesData: movieResponse.data
+      })
+
+    }
+    catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: `An Error Occurred: ${error.response.status}`
+      });
+    }
+  }
+
   render() {
     return (
       <>
@@ -91,6 +118,7 @@ class App extends React.Component {
             </ul>
             <img alt='' src={this.state.cityMap}></img>
             <Weather weatherData={this.state.weatherData} />
+            {this.state.moviesData.length && <Movies moviesData={this.state.moviesData} />}
           </>
           : ''
 
