bd6f267088e90e131831326a440cc2c7a4b421d8
Rui Guo <guojiarui@gmail.com>
2022-06-26 23:35:25 -0700

    more cleanup, WeatherDay.js, Movie.js edited

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import WeatherDay from './WeatherDay';
 // import WeatherDay from './WeatherDay';
 class Weather extends React.Component {
 
